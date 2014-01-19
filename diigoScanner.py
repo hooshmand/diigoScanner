7523b2c594bb2b4d3fb9b267da6b04987ab21df0
Hooshmand <hooshmand.k@gmail.com>
2014-01-19 18:29:04 +0330

    first version

---
+++ diigoScanner.py
@@ -0,0 +1,42 @@
+# Created by @hooshmandk - http://scriptics.ir
+from urllib.request import *
+from urllib.error import *
+from urllib.parse import *
+import re
+
+
+def send(uemail):
+    try:
+        print("[+] Sending request...");
+        params = {'username': uemail}
+        data = urlencode(params).encode('utf-8')
+        request = Request("https://www.diigo.com/user_mana2/forgot")
+        #Send request and analyse response
+        f = urlopen(request, data)
+        response = f.read().decode('utf-8')
+        e = re.compile('<strong>(.*?)</strong>')
+        mch = e.findall(response)
+        out = open("out.txt","a")
+        if mch :
+            print(uemail + " : " + mch[0])
+            out.write(uemail + " : " + mch[0] + "\n")
+        else :
+            print(uemail + " : " + "No account")
+            #out.write(uemail + " : " + "No account" + "\n")
+
+        out.close()
+
+    except Exception as e:
+        print("Error: " + str(e))
+
+
+print("[+] Start!");
+#Send request to site
+fname = 'maillist.txt'
+f= open(fname);
+lines = f.readlines();
+for i in lines:
+    send(i.strip());
+#send('hooshmand.k@gmail.com');
+
+print("[+] Finished!");
